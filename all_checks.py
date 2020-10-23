0bf361acbb71ac7d30807097ab711c64439dfe21
yaskarimi <ykarimi.2312@gmail.com>
2020-10-23 10:36:11 -0700

    fixed syntax

--- all_checks.py
+++ all_checks.py
@@ -12,7 +12,8 @@ def check_reboot():
 def check_disk_full(disk, min_gb , min_percent):
 
   du = shutil.disk_usage(disk)
-  percenr_free = du.free / 2**30 
+  percent_free = 100 * du.free / du.total
+  gigabytes_free = du.free / 2**30 
   if gigabytes_free < min_gb or percent_free < min_percent :
    return True
   return False
@@ -25,7 +26,7 @@ def main():
    if check_reboot():
      print("pending Reboot.")
      sys.exit(1)
-   if check_root__full():
+   if check_root_full():
      print("root partition full")
      sys.exit(1)
    print("Everything ok")
